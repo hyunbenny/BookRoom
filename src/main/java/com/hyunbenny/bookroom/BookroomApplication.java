bfe1268f4885600a514ae592fcff57c4f07e9c24
hyunbenny <bluesky3268@naver.com>
2023-09-04 10:45:29 +0900

    initial commit

---
+++ src/main/java/com/hyunbenny/bookroom/BookroomApplication.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.bookroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookroomApplication.class, args);
+	}
+
+}
